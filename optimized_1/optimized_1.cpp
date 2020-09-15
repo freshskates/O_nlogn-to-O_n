5fe82ad14f880bc0993f4cefa9d6ebb3436f8b6f
Robert <67438520+freshskates@users.noreply.github.com>
2020-09-14 22:47:09 -0700

    new

--- optimized_1/optimized_1.cpp
+++ optimized_1/optimized_1.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
+#include <ctime>
 using namespace std; 
+
+void print_s(int n, string s) {
+	int ans = (((n + 1) * ceil(log2(n + 1))) - pow(2, ceil(log2(n + 1))) + 1);
+	for (int i = 1; i <= ans; i++)
+		cout << s << " | " << i << endl;
+}
+
+void print_s2(int n, string s) {
+	int count = 0;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j = j* 2) {
+			cout << s << endl ;
+			count++; 
+		} 
+	}
+	cout << count << endl; 
+}
+int main(){
+	clock_t begin = clock();
+	print_s(10, "hello");
+	clock_t end = clock();
+
+	clock_t begin2 = clock();
+	print_s2(10, "hello");
+	clock_t end2 = clock();
+
+	double process1 =  (double)((double)end - (double)begin) / CLOCKS_PER_SEC;
+	double process2 = (double)((double)end2 - (double)begin2) / CLOCKS_PER_SEC;
+	cout << "Process(1) Time(s): " << process1 << "s"<< endl ; 
+	cout << "Process(1) Time(ms): " << 1000 * process1 << "ms" << endl;
+	cout << "Process(2) Time(s): " << process2 << "s" << endl;
+	cout << "Process(2) Time(ms): " << 1000 * process2 << "ms" << endl;
+} 
+
 
 /*
 Non Optimized
@@ -10,12 +45,3 @@ for(int i =0; i < n; i++){
 	}
 }
 */
-void print_s(int n, string s) {
-	int ans = (((n + 1) * ceil(log2(n + 1))) - pow(2, ceil(log2(n + 1))) + 1);
-	for (int i = 1; i <= ans; i++) 
-		cout << s << " | " << i << endl;
-}
-
-int main(){
-	print_s(4, "hello");
-} 
